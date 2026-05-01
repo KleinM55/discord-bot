02491e006726eb03ca5549b8011e07297127b347
KleinM55 <aa5976872@gmail.com>
2026-05-01 20:55:05 +0300

    Update take.js

--- commands/take.js
+++ commands/take.js
@@ -1,27 +1,28 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const eco = require('../utils/economy');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('take')
-    .setDescription('Remove crops')
+    .setDescription('سحب محصول (إداري)')
     .addUserOption(opt =>
       opt.setName('user').setRequired(true)
     )
     .addIntegerOption(opt =>
       opt.setName('amount').setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    ),
 
   async execute(interaction) {
     const user = interaction.options.getUser('user');
     const amount = interaction.options.getInteger('amount');
 
-    eco.removeFarm(user.id, amount);
+    eco.removeBalance(user.id, amount);
 
-    await interaction.reply({
-      content: `❌ Removed ${amount} crops`,
-      flags: 64
-    });
+    const embed = new EmbedBuilder()
+      .setTitle('❌ تم السحب')
+      .setDescription(`تم سحب **${amount} محصول** من ${user.username}`)
+      .setColor(0xe74c3c);
+
+    await interaction.reply({ embeds: [embed] });
   }
 };
