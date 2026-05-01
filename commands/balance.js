e9c1eb2040000d71b85675e4d03f6bd6655a99ce
KleinM55 <aa5976872@gmail.com>
2026-05-01 18:58:30 +0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,18 +4,22 @@ const eco = require('../utils/economy');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('balance')
-    .setDescription('Check your balance'),
+    .setDescription('Check farm')
+    .addUserOption(opt =>
+      opt.setName('user')
+        .setDescription('Target user')
+        .setRequired(false)
+    ),
 
   async execute(interaction) {
-    const user = eco.getUser(interaction.user.id);
+    const target = interaction.options.getUser('user') || interaction.user;
+    const user = eco.getUser(target.id);
 
     const embed = new EmbedBuilder()
-      .setTitle('💰 رصيدك')
-      .setDescription(`💵 الرصيد: **${user.balance}**\n🌾 المحصول: **${user.farm}**`)
-      .setColor(0x3498db)
-      .setFooter({
-        text: `مزرعة السحر 🌿 | ${new Date().toLocaleString()}`
-      });
+      .setTitle('🌾 Farm Info')
+      .setDescription(`🌿 Crops: **${user.farm}**`)
+      .setColor(0x2ecc71)
+      .setFooter({ text: `Magic Farm 🌿 | ${new Date().toLocaleString()}` });
 
     await interaction.reply({ embeds: [embed] });
   }
